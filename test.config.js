6e02ad9a61e5fbe8e4499f1d652bce5cedec472b
Chris Dolphin <cdolphin1991@gmail.com>
2018-05-18 12:55:09 -0700

    Update dependencies and fix raf issue

--- test.config.js
+++ test.config.js
@@ -1,3 +1,7 @@
+// TODO: Remove this `raf` polyfill once the below issue is sorted
+// https://github.com/facebookincubator/create-react-app/issues/3199#issuecomment-332842582
+import 'raf/polyfill'
+
 const Enzyme = require('enzyme');
 const EnzymeAdapter = require('enzyme-adapter-react-16');
 
